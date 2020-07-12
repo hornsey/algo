cf292a3e898c69b18acf2aac073681fcde31588a
huangtao <huangtao@czb365.com>
2020-07-12 12:46:59 +0800

    q46

---
+++ src/main/java/com/hornsey/algo/leetcode/Q46Solution.java
@@ -0,0 +1,49 @@
+package com.hornsey.algo.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author huangtao
+ * @date 2020/7/12
+ */
+public class Q46Solution {
+	public void backtrack(int n,
+	                      ArrayList<Integer> output,
+	                      List<List<Integer>> res,
+	                      int first) {
+		// 所有数都填完了
+		if (first == n) {
+			res.add(new ArrayList<Integer>(output));
+		}
+		for (int i = first; i < n; i++) {
+			// 动态维护数组
+			Collections.swap(output, first, i);
+			// 继续递归填下一个数
+			backtrack(n, output, res, first + 1);
+			// 撤销操作
+			Collections.swap(output, first, i);
+		}
+	}
+
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> res = new LinkedList();
+
+		ArrayList<Integer> output = new ArrayList<Integer>();
+		for (int num : nums) {
+			output.add(num);
+		}
+
+		int n = nums.length;
+		backtrack(n, output, res, 0);
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3};
+		new Q46Solution().permute(nums);
+	}
+
+}
