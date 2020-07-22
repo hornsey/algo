33f2bfd5dd334d98579d4eb8e36ba3c0af865213
huangtao <huangtao@czb365.com>
2020-07-22 09:30:20 +0800

    leetcode Q78 v3

--- src/main/java/com/hornsey/algo/leetcode/Q78Solution.java
+++ src/main/java/com/hornsey/algo/leetcode/Q78Solution.java
@@ -6,20 +6,25 @@ import java.util.List;
 /**
  * @author huangtao
  * @date 2020/7/20
+ * @version 3
  */
 public class Q78Solution {
 	public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> res = new ArrayList<>();
-		res.add(new ArrayList<>());
 
-		for (Integer num : nums) {
-			List<List<Integer>> curRes = new ArrayList<>(res);
-			for (List<Integer> list : curRes) {
-				res.add(new ArrayList<Integer>(list){{add(num);}});
-			}
-		}
+		backtrace(nums, 0, new ArrayList<>(), res);
 
 		return res;
+	}
+
+	private void backtrace(int[] nums, int start,  List<Integer> cur, List<List<Integer>> res) {
+		res.add(new ArrayList<>(cur));
+
+		for (int i = start; i < nums.length; i++) {
+			cur.add(nums[i]);
+			backtrace(nums, i+1, cur, res);
+			cur.remove(cur.size()-1);
+		}
 	}
 
 	public static void main(String[] args) {
