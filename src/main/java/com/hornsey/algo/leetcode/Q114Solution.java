4a63454a4c58eca482ef56faa1399b0ee0a1b09f
huangtao <huangtao@czb365.com>
2020-08-03 23:04:36 +0800

    leetcode Q114 v2

--- src/main/java/com/hornsey/algo/leetcode/Q114Solution.java
+++ src/main/java/com/hornsey/algo/leetcode/Q114Solution.java
@@ -1,11 +1,14 @@
 package com.hornsey.algo.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
  * @author huangtao
  * @date 2020/8/2
+ * @version 2
  */
 public class Q114Solution {
 	public void flatten(TreeNode root) {
@@ -13,27 +16,22 @@ public class Q114Solution {
 			return;
 		}
 
-		Queue<TreeNode> queue = new LinkedList<>();
-		dfs(root, queue);
-		queue.poll();
-		root.left = null;
+		List<TreeNode> list = new ArrayList<>();
+		dfs(root, list);
 
-		TreeNode tmp;
-		TreeNode tail = root;
-		while (!queue.isEmpty()) {
-			tmp = queue.poll();
-			tmp.left = null;
-			tail.right = tmp;
-			tail = tmp;
+		for (int i = 1; i < list.size(); i++) {
+			TreeNode pre = list.get(i-1);
+			pre.left = null;
+			pre.right = list.get(i);
 		}
 	}
 
-	private void dfs(TreeNode root, Queue<TreeNode> queue) {
+	private void dfs(TreeNode root, List<TreeNode> list) {
 		if (root == null) {
 			return;
 		}
-		queue.offer(root);
-		dfs(root.left, queue);
-		dfs(root.right, queue);
+		list.add(root);
+		dfs(root.left, list);
+		dfs(root.right, list);
 	}
 }
