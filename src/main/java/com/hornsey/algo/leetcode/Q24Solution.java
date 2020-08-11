c9b7697c7bd5fcd5322673e7443d0ce47fa2d416
hornsey <hornseyhuang@163.com>
2020-08-12 00:15:14 +0800

    leetcode Q24 v2

--- src/main/java/com/hornsey/algo/leetcode/Q24Solution.java
+++ src/main/java/com/hornsey/algo/leetcode/Q24Solution.java
@@ -3,24 +3,17 @@ package com.hornsey.algo.leetcode;
 /**
  * @author huangtao
  * @date 2020/8/11
+ * @version v2 递归
  */
 public class Q24Solution {
 	public ListNode swapPairs(ListNode head) {
 		if (head == null || head.next == null) {
 			return head;
 		}
-		ListNode pre = new ListNode(-1);
-		pre.next = head;
-		ListNode tmp = pre;
-		while (tmp.next != null && tmp.next.next != null) {
-			ListNode first = tmp.next;
-			ListNode second = tmp.next.next;
+		ListNode next = head.next;
+		head.next = swapPairs(next.next);
+		next.next = head;
 
-			tmp.next = second;
-			first.next = second.next;
-			second.next = first;
-			tmp = first;
-		}
-		return pre.next;
+		return next;
 	}
 }
