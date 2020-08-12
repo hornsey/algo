b3d101a270798c309bbdd0ba9d9d04fe2390fff5
hornsey <hornseyhuang@163.com>
2020-08-13 07:05:43 +0800

    leetcode Q43 v2

--- src/main/java/com/hornsey/algo/leetcode/Q43Solution.java
+++ src/main/java/com/hornsey/algo/leetcode/Q43Solution.java
@@ -1,8 +1,11 @@
 package com.hornsey.algo.leetcode;
 
 /**
+ * 优化竖版
+ *
  * @author huangtao
  * @date 2020/8/13
+ * @version 2
  */
 public class Q43Solution {
 	public String multiply(String num1, String num2) {
@@ -10,37 +13,26 @@ public class Q43Solution {
 			return "0";
 		}
 
-		String res = "0";
-
-		for (int i = num2.length()-1; i>=0; i--) {
-			int carry = 0;
-			StringBuilder temp = new StringBuilder();
-			for (int j = 0; j < num2.length() - 1 - i; j++) {
-				temp.append(0);
+		int len1 = num1.length(), len2 = num2.length();
+		int[] res = new int[len1+len2];
+		for (int i = len1-1; i >= 0; i--) {
+			int n1 = num1.charAt(i) - '0';
+			for (int j = len2-1; j>=0; j--) {
+				int n2 = num2.charAt(j) - '0';
+				int sum = res[i+j+1] + n1*n2;
+				res[i+j+1] = sum % 10;
+				res[i+j] += sum/10;
 			}
-			int n2 = num2.charAt(i) - '0';
-
-			for (int j = num1.length()-1; j>=0||carry != 0; j--) {
-				int n1 = j < 0 ? 0 : num1.charAt(j) - '0';
-				int product = n1*n2 + carry;
-				temp.append(product % 10);
-				carry = product / 10;
-			}
-			res = addStrings(res, temp.reverse().toString());
 		}
-		return res;
+
+		StringBuilder temp = new StringBuilder();
+		for (int i = 0; i < res.length; i++) {
+			if (i == 0 && res[i] == 0) {
+				continue;
+			}
+			temp.append(res[i]);
+		}
+		return temp.toString();
 	}
 
-	private String addStrings(String num1, String num2) {
-		StringBuilder sb = new StringBuilder();
-		int carry = 0;
-		for (int i = num1.length()-1, j = num2.length()-1; i>=0||j>=0||carry!=0; i--,j--) {
-			int x = i < 0 ? 0 : num1.charAt(i) - '0';
-			int y = j < 0 ? 0 : num2.charAt(j) - '0';
-			int sum = (x + y + carry);
-			sb.append(sum%10);
-			carry = sum / 10;
-		}
-		return sb.reverse().toString();
-	}
 }
