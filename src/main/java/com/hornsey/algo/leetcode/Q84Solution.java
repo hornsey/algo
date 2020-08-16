5d78899ee5f7efa9b417600dafd88a84c853be1e
hornsey <hornseyhuang@163.com>
2020-08-16 18:08:13 +0800

    leetcode Q84 v2

--- src/main/java/com/hornsey/algo/leetcode/Q84Solution.java
+++ src/main/java/com/hornsey/algo/leetcode/Q84Solution.java
@@ -1,24 +1,32 @@
 package com.hornsey.algo.leetcode;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * @author huangtao
  * @date 2020/8/16
- * @version 1
+ * @version 2
  */
 public class Q84Solution {
 	public int largestRectangleArea(int[] heights) {
 		int area = 0;
 		int n = heights.length;
+		int[] l = new int[n];
+		int[] r = new int[n];
+		Arrays.fill(r, n);
+
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+				r[stack.pop()] = i;
+			}
+			l[i] = stack.isEmpty() ? -1 : stack.peek();
+			stack.push(i);
+		}
 
 		for (int i = 0; i < n; i++) {
-			int l = i, r = i;
-			while (l - 1 >= 0 && heights[l-1] >= heights[i]) {
-				l--;
-			}
-			while (r + 1 < n && heights[r+1] >= heights[i]) {
-				r++;
-			}
-			area = Math.max(area, (r-l+1)*heights[i]);
+			area = Math.max(area, (r[i]- l[i] - 1)*heights[i]);
 		}
 		return area;
 	}
